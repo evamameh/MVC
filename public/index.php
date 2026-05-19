e0e4bade6fa0746fdebdac0a515d24418acfba9d
Eva Mae Conag <120082846+evamameh@users.noreply.github.com>
2026-05-19 10:07:20 +0800

    Update index.php
    Removed unnecessary blank lines for cleaner code.

--- public/index.php
+++ public/index.php
@@ -1,21 +1,10 @@
 <?php
-
 declare(strict_types=1);
-
-
-
-
-
 
 use Core\Application;
 
-
 session_start();
 
-
-
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-
 
 Application::bootstrap(dirname(__DIR__))->run();
